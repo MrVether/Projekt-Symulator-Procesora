5f0b74af16c6cbc9f97f54c6740402b511661315
MrVether <80617876+MrVether@users.noreply.github.com>
2021-08-01 15:53:38 +0200

    Add project files.

---
+++ Projetk1/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace Projetk1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1a = new System.Windows.Forms.TextBox();
+            this.textBox2b = new System.Windows.Forms.TextBox();
+            this.textBox3c = new System.Windows.Forms.TextBox();
+            this.textBox4d = new System.Windows.Forms.TextBox();
+            this.blad1 = new System.Windows.Forms.TextBox();
+            this.MOV_AX_BX = new System.Windows.Forms.Button();
+            this.MOV_AX_CX = new System.Windows.Forms.Button();
+            this.MOV_AX_DX = new System.Windows.Forms.Button();
+            this.MOV_BX_AX = new System.Windows.Forms.Button();
+            this.MOV_BX_CX = new System.Windows.Forms.Button();
+            this.MOV_BX_DX = new System.Windows.Forms.Button();
+            this.MOV_CX_AX = new System.Windows.Forms.Button();
+            this.MOV_CX_BX = new System.Windows.Forms.Button();
+            this.MOV_CX_DX = new System.Windows.Forms.Button();
+            this.MOV_DX_AX = new System.Windows.Forms.Button();
+            this.MOV_DX_BX = new System.Windows.Forms.Button();
+            this.MOV_DX_CX = new System.Windows.Forms.Button();
+            this.XCHG_AX_BX = new System.Windows.Forms.Button();
+            this.XCHG_AX_CX = new System.Windows.Forms.Button();
+            this.XCHG_AX_DX = new System.Windows.Forms.Button();
+            this.XCHG_BX_AX = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(374, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1a
+            // 
+            this.textBox1a.Location = new System.Drawing.Point(36, 118);
+            this.textBox1a.Name = "textBox1a";
+            this.textBox1a.Size = new System.Drawing.Size(374, 27);
+            this.textBox1a.TabIndex = 1;
+            // 
+            // textBox2b
+            // 
+            this.textBox2b.Location = new System.Drawing.Point(36, 151);
+            this.textBox2b.Name = "textBox2b";
+            this.textBox2b.Size = new System.Drawing.Size(374, 27);
+            this.textBox2b.TabIndex = 2;
+            // 
+            // textBox3c
+            // 
+            this.textBox3c.Location = new System.Drawing.Point(36, 184);
+            this.textBox3c.Name = "textBox3c";
+            this.textBox3c.Size = new System.Drawing.Size(374, 27);
+            this.textBox3c.TabIndex = 3;
+            // 
+            // textBox4d
+            // 
+            this.textBox4d.Location = new System.Drawing.Point(36, 217);
+            this.textBox4d.Name = "textBox4d";
+            this.textBox4d.Size = new System.Drawing.Size(374, 27);
+            this.textBox4d.TabIndex = 4;
+            // 
+            // blad1
+            // 
+            this.blad1.Location = new System.Drawing.Point(36, 85);
+            this.blad1.Name = "blad1";
+            this.blad1.Size = new System.Drawing.Size(374, 27);
+            this.blad1.TabIndex = 5;
+            this.blad1.Text = "Brak błędu";
+            // 
+            // MOV_AX_BX
+            // 
+            this.MOV_AX_BX.Location = new System.Drawing.Point(36, 277);
+            this.MOV_AX_BX.Name = "MOV_AX_BX";
+            this.MOV_AX_BX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_AX_BX.TabIndex = 6;
+            this.MOV_AX_BX.Text = "MOV AX, BX";
+            this.MOV_AX_BX.UseVisualStyleBackColor = true;
+            this.MOV_AX_BX.Click += new System.EventHandler(this.MOV_AX_BX_Click);
+            // 
+            // MOV_AX_CX
+            // 
+            this.MOV_AX_CX.Location = new System.Drawing.Point(36, 312);
+            this.MOV_AX_CX.Name = "MOV_AX_CX";
+            this.MOV_AX_CX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_AX_CX.TabIndex = 7;
+            this.MOV_AX_CX.Text = "MOV AX, CX";
+            this.MOV_AX_CX.UseVisualStyleBackColor = true;
+            this.MOV_AX_CX.Click += new System.EventHandler(this.MOV_AX_CX_Click);
+            // 
+            // MOV_AX_DX
+            // 
+            this.MOV_AX_DX.Location = new System.Drawing.Point(36, 347);
+            this.MOV_AX_DX.Name = "MOV_AX_DX";
+            this.MOV_AX_DX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_AX_DX.TabIndex = 8;
+            this.MOV_AX_DX.Text = "MOV AX, DX";
+            this.MOV_AX_DX.UseVisualStyleBackColor = true;
+            this.MOV_AX_DX.Click += new System.EventHandler(this.MOV_AX_DX_Click);
+            // 
+            // MOV_BX_AX
+            // 
+            this.MOV_BX_AX.Location = new System.Drawing.Point(36, 382);
+            this.MOV_BX_AX.Name = "MOV_BX_AX";
+            this.MOV_BX_AX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_BX_AX.TabIndex = 9;
+            this.MOV_BX_AX.Text = "MOV BX, AX";
+            this.MOV_BX_AX.UseVisualStyleBackColor = true;
+            this.MOV_BX_AX.Click += new System.EventHandler(this.MOV_BX_AX_Click);
+            // 
+            // MOV_BX_CX
+            // 
+            this.MOV_BX_CX.Location = new System.Drawing.Point(36, 418);
+            this.MOV_BX_CX.Name = "MOV_BX_CX";
+            this.MOV_BX_CX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_BX_CX.TabIndex = 10;
+            this.MOV_BX_CX.Text = "MOV BX, CX";
+            this.MOV_BX_CX.UseVisualStyleBackColor = true;
+            this.MOV_BX_CX.Click += new System.EventHandler(this.MOV_BX_CX_Click);
+            // 
+            // MOV_BX_DX
+            // 
+            this.MOV_BX_DX.Location = new System.Drawing.Point(36, 453);
+            this.MOV_BX_DX.Name = "MOV_BX_DX";
+            this.MOV_BX_DX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_BX_DX.TabIndex = 11;
+            this.MOV_BX_DX.Text = "MOV BX, DX";
+            this.MOV_BX_DX.UseVisualStyleBackColor = true;
+            this.MOV_BX_DX.Click += new System.EventHandler(this.MOV_BX_DX_Click);
+            // 
+            // MOV_CX_AX
+            // 
+            this.MOV_CX_AX.Location = new System.Drawing.Point(36, 488);
+            this.MOV_CX_AX.Name = "MOV_CX_AX";
+            this.MOV_CX_AX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_CX_AX.TabIndex = 12;
+            this.MOV_CX_AX.Text = "MOV CX, AX";
+            this.MOV_CX_AX.UseVisualStyleBackColor = true;
+            this.MOV_CX_AX.Click += new System.EventHandler(this.MOV_CX_AX_Click);
+            // 
+            // MOV_CX_BX
+            // 
+            this.MOV_CX_BX.Location = new System.Drawing.Point(36, 524);
+            this.MOV_CX_BX.Name = "MOV_CX_BX";
+            this.MOV_CX_BX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_CX_BX.TabIndex = 13;
+            this.MOV_CX_BX.Text = "MOV CX, BX";
+            this.MOV_CX_BX.UseVisualStyleBackColor = true;
+            this.MOV_CX_BX.Click += new System.EventHandler(this.MOV_CX_BX_Click);
+            // 
+            // MOV_CX_DX
+            // 
+            this.MOV_CX_DX.Location = new System.Drawing.Point(36, 559);
+            this.MOV_CX_DX.Name = "MOV_CX_DX";
+            this.MOV_CX_DX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_CX_DX.TabIndex = 14;
+            this.MOV_CX_DX.Text = "MOV CX, DX";
+            this.MOV_CX_DX.UseVisualStyleBackColor = true;
+            this.MOV_CX_DX.Click += new System.EventHandler(this.MOV_CX_DX_Click);
+            // 
+            // MOV_DX_AX
+            // 
+            this.MOV_DX_AX.Location = new System.Drawing.Point(36, 594);
+            this.MOV_DX_AX.Name = "MOV_DX_AX";
+            this.MOV_DX_AX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_DX_AX.TabIndex = 15;
+            this.MOV_DX_AX.Text = "MOV DX, AX";
+            this.MOV_DX_AX.UseVisualStyleBackColor = true;
+            this.MOV_DX_AX.Click += new System.EventHandler(this.MOV_DX_AX_Click);
+            // 
+            // MOV_DX_BX
+            // 
+            this.MOV_DX_BX.Location = new System.Drawing.Point(36, 629);
+            this.MOV_DX_BX.Name = "MOV_DX_BX";
+            this.MOV_DX_BX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_DX_BX.TabIndex = 16;
+            this.MOV_DX_BX.Text = "MOV DX, BX";
+            this.MOV_DX_BX.UseVisualStyleBackColor = true;
+            this.MOV_DX_BX.Click += new System.EventHandler(this.MOV_DX_BX_Click);
+            // 
+            // MOV_DX_CX
+            // 
+            this.MOV_DX_CX.Location = new System.Drawing.Point(36, 664);
+            this.MOV_DX_CX.Name = "MOV_DX_CX";
+            this.MOV_DX_CX.Size = new System.Drawing.Size(173, 29);
+            this.MOV_DX_CX.TabIndex = 17;
+            this.MOV_DX_CX.Text = "MOV DX, CX";
+            this.MOV_DX_CX.UseVisualStyleBackColor = true;
+            this.MOV_DX_CX.Click += new System.EventHandler(this.MOV_DX_CX_Click);
+            // 
+            // XCHG_AX_BX
+            // 
+            this.XCHG_AX_BX.Location = new System.Drawing.Point(215, 277);
+            this.XCHG_AX_BX.Name = "XCHG_AX_BX";
+            this.XCHG_AX_BX.Size = new System.Drawing.Size(195, 29);
+            this.XCHG_AX_BX.TabIndex = 18;
+            this.XCHG_AX_BX.Text = "XCHG AX, BX";
+            this.XCHG_AX_BX.UseVisualStyleBackColor = true;
+            this.XCHG_AX_BX.Click += new System.EventHandler(this.XCHG_AX_BX_Click);
+            // 
+            // XCHG_AX_CX
+            // 
+            this.XCHG_AX_CX.Location = new System.Drawing.Point(215, 312);
+            this.XCHG_AX_CX.Name = "XCHG_AX_CX";
+            this.XCHG_AX_CX.Size = new System.Drawing.Size(195, 29);
+            this.XCHG_AX_CX.TabIndex = 19;
+            this.XCHG_AX_CX.Text = "XCHG AX, CX";
+            this.XCHG_AX_CX.UseVisualStyleBackColor = true;
+            this.XCHG_AX_CX.Click += new System.EventHandler(this.XCHG_AX_CX_Click);
+            // 
+            // XCHG_AX_DX
+            // 
+            this.XCHG_AX_DX.Location = new System.Drawing.Point(216, 347);
+            this.XCHG_AX_DX.Name = "XCHG_AX_DX";
+            this.XCHG_AX_DX.Size = new System.Drawing.Size(194, 29);
+            this.XCHG_AX_DX.TabIndex = 20;
+            this.XCHG_AX_DX.Text = "XCHG AX, DX";
+            this.XCHG_AX_DX.UseVisualStyleBackColor = true;
+            this.XCHG_AX_DX.Click += new System.EventHandler(this.XCHG_AX_DX_Click);
+            // 
+            // XCHG_BX_AX
+            // 
+            this.XCHG_BX_AX.Location = new System.Drawing.Point(216, 382);
+            this.XCHG_BX_AX.Name = "XCHG_BX_AX";
+            this.XCHG_BX_AX.Size = new System.Drawing.Size(194, 29);
+            this.XCHG_BX_AX.TabIndex = 21;
+            this.XCHG_BX_AX.Text = "XCHG BX, AX";
+            this.XCHG_BX_AX.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1799, 809);
+            this.Controls.Add(this.XCHG_BX_AX);
+            this.Controls.Add(this.XCHG_AX_DX);
+            this.Controls.Add(this.XCHG_AX_CX);
+            this.Controls.Add(this.XCHG_AX_BX);
+            this.Controls.Add(this.MOV_DX_CX);
+            this.Controls.Add(this.MOV_DX_BX);
+            this.Controls.Add(this.MOV_DX_AX);
+            this.Controls.Add(this.MOV_CX_DX);
+            this.Controls.Add(this.MOV_CX_BX);
+            this.Controls.Add(this.MOV_CX_AX);
+            this.Controls.Add(this.MOV_BX_DX);
+            this.Controls.Add(this.MOV_BX_CX);
+            this.Controls.Add(this.MOV_BX_AX);
+            this.Controls.Add(this.MOV_AX_DX);
+            this.Controls.Add(this.MOV_AX_CX);
+            this.Controls.Add(this.MOV_AX_BX);
+            this.Controls.Add(this.blad1);
+            this.Controls.Add(this.textBox4d);
+            this.Controls.Add(this.textBox3c);
+            this.Controls.Add(this.textBox2b);
+            this.Controls.Add(this.textBox1a);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Symulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1a;
+        private System.Windows.Forms.TextBox textBox2b;
+        private System.Windows.Forms.TextBox textBox3c;
+        private System.Windows.Forms.TextBox textBox4d;
+        private System.Windows.Forms.TextBox blad1;
+        private System.Windows.Forms.Button MOV_AX_BX;
+        private System.Windows.Forms.Button MOV_AX_CX;
+        private System.Windows.Forms.Button MOV_AX_DX;
+        private System.Windows.Forms.Button MOV_BX_AX;
+        private System.Windows.Forms.Button MOV_BX_CX;
+        private System.Windows.Forms.Button MOV_BX_DX;
+        private System.Windows.Forms.Button MOV_CX_AX;
+        private System.Windows.Forms.Button MOV_CX_BX;
+        private System.Windows.Forms.Button MOV_CX_DX;
+        private System.Windows.Forms.Button MOV_DX_AX;
+        private System.Windows.Forms.Button MOV_DX_BX;
+        private System.Windows.Forms.Button MOV_DX_CX;
+        private System.Windows.Forms.Button XCHG_AX_BX;
+        private System.Windows.Forms.Button XCHG_AX_CX;
+        private System.Windows.Forms.Button XCHG_AX_DX;
+        private System.Windows.Forms.Button XCHG_BX_AX;
+    }
+}
+
